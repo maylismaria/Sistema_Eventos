2df3ba4640a16356696cda9375661efb62ad34d6
Maylis Maria <maylis.maria15@gmail.com>
2025-06-12 17:36:27 -0300

    Participant

---
+++ src/main/java/devsupeior/SistemaEventos/entities/Participant.java
@@ -0,0 +1,54 @@
+package devsupeior.SistemaEventos.entities;
+
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_participant")
+public class Participant {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+
+    public Participant(){
+
+    }
+
+    public Participant(Long id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+}
