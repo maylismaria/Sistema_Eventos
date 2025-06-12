2c478de70505554bc14c73bfeb28fdf187f36505
Maylis Maria <maylis.maria15@gmail.com>
2025-06-12 15:04:57 -0300

    Primeiro commit

---
+++ src/test/java/devsupeior/SistemaEventos/SistemaEventosApplicationTests.java
@@ -0,0 +1,13 @@
+package devsupeior.SistemaEventos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaEventosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
